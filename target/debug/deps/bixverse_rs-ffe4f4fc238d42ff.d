83e766eb2f59bb74ab025715c1af0b2b7be0ce9f
Gregor Lueg <gregor@phenomic.ai>
2026-01-19 09:07:41 +0100

    feat: initialisation and git ignore set up

--- target/debug/deps/bixverse_rs-ffe4f4fc238d42ff.d
+++
@@ -1,9 +0,0 @@
-/Users/gregorlueg/repos/shared/bixverse-rs/target/debug/deps/bixverse_rs-ffe4f4fc238d42ff.d: src/main.rs Cargo.toml
-
-/Users/gregorlueg/repos/shared/bixverse-rs/target/debug/deps/libbixverse_rs-ffe4f4fc238d42ff.rmeta: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
