7f40f1688d61cb6c1d9924f6c8507c5ea8695686
Gregor Lueg <gregor@phenomic.ai>
2026-01-19 09:05:02 +0100

    feat: ci/cd set up

---
+++ target/debug/deps/bixverse_rs-28f62e5989cbfdba.d
@@ -0,0 +1,9 @@
+/Users/gregorlueg/repos/shared/bixverse-rs/target/debug/deps/bixverse_rs-28f62e5989cbfdba.d: src/main.rs Cargo.toml
+
+/Users/gregorlueg/repos/shared/bixverse-rs/target/debug/deps/libbixverse_rs-28f62e5989cbfdba.rmeta: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
