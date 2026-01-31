391e90b6100104d2c42abe462e19053db8967521
Gregor Lueg <gregor@phenomic.ai>
2026-01-31 20:50:41 +0100

    feat: expose rust r interface via prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -4,6 +4,7 @@ pub use crate::core::math::sparse::{
 pub use crate::graph::graph_structures::{EdgeData, NodeData, SparseGraph};
 pub use crate::utils::heap_structures::OrderedFloat;
 pub use crate::utils::matrix_utils::*;
+pub use crate::utils::r_rust_interface::*;
 pub use crate::utils::traits::*;
 pub use crate::utils::vec_utils::*;
 pub use crate::{assert_nrows, assert_same_len, assert_symmetric_mat};
