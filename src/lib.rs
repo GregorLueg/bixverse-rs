5868c70191463bdaec9d107e38a4a268c2a83714
Gregor Lueg <gregor@phenomic.ai>
2026-03-08 15:25:53 +0100

    feat: scenic implementation with gene batching and sparse predictor variables

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
+//! This crate contains highly optimised code for computational biology. It
+//! has various statistical and computational biology methods, written with
+//! performance in mind. Additionally, it now also has a first take at a single
+//! cell suite of methods and functions.
+
 #![allow(clippy::needless_range_loop)]
 #![warn(missing_docs)]
 
 pub mod core;
-pub mod data;
 pub mod enrichment;
 pub mod graph;
 pub mod methods;
