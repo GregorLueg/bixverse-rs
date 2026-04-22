39894acff9ef25a12f4cc57156b598b565b0fbfd
Gregor Lueg <gregor@phenomic.ai>
2026-04-22 15:53:18 +0200

    feat: rust to r error

--- src/errors.rs
+++ src/errors.rs
@@ -163,3 +163,10 @@ pub enum BixverseErrors {
     #[error("Invalid model type: {0}")]
     HotSpotWrongModel(String),
 }
+
+/// Transform into an R extendr API error
+impl From<BixverseErrors> for extendr_api::Error {
+    fn from(err: BixverseErrors) -> Self {
+        extendr_api::Error::Other(err.to_string())
+    }
+}
