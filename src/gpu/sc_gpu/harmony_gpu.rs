3001002bc567ca62c8cfae0576378a776891c94b
Gregor Lueg <gregorlueg@me.com>
2026-07-26 23:13:34 +0200

    fix(harmony): default k to 100

--- src/gpu/sc_gpu/harmony_gpu.rs
+++ src/gpu/sc_gpu/harmony_gpu.rs
@@ -99,7 +99,7 @@ pub struct HarmonyParamsV2Gpu {
 impl Default for HarmonyParamsV2Gpu {
     fn default() -> Self {
         Self {
-            k: 10,
+            k: 100,
             sigma: vec![0.1],
             theta: vec![2.0],
             lambda: vec![1.0],
